0915d3f886c77788e9ed7eaf8de392b86ae6dee9
umar-qureshi2 <umar.mcs@gmail.com>
2017-12-31 11:14:22 +0400

    Initial commit - Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            // Declare a variable named 'inputString' to hold our input.
+            String inputString;
+
+            // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+            inputString = Console.ReadLine();
+
+            // Print a string literal saying "Hello, World." to stdout using cout.
+            Console.WriteLine("Hello, World.");
+            Console.WriteLine(inputString);
+            
+        }
+    }
+}
